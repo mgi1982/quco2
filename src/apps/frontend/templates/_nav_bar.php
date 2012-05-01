83a5eb5d55284d480e2bb712415b3c198b30a034
Marcos Ibañez <mgi1982@gmail.com>
2012-05-01 02:25:11 -0300

    Implemented layout, authentication and nav-bar

---
+++ src/apps/frontend/templates/_nav_bar.php
@@ -0,0 +1,29 @@
+<?php use_helper('I18N'); ?>
+<div class="navbar navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container-fluid">
+      <a class="btn btn-navbar" data-toggle="collapse"
+        data-target=".nav-collapse"> <span class="icon-bar"></span> <span
+        class="icon-bar"></span> <span class="icon-bar"></span>
+      </a> <a class="brand" href="#">Quco2</a>
+      <?php if($sf_user->isAuthenticated()): ?>
+      <div class="btn-group pull-right">
+        <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+          <i class="icon-user"></i> <?php echo $sf_user ?> <span class="caret"></span>
+        </a>
+        <ul class="dropdown-menu">
+          <li><?php echo link_to(__('Logout'), '@sf_guard_signout')?></li>
+        </ul>
+      </div>
+      <div class="nav-collapse">
+        <ul class="nav">
+        <?php foreach(MetricQuery::create()->find() as $metric): ?>
+          <li><?php echo link_to($metric->getName(), '/')?></li>
+        <?php endforeach;?>
+        </ul>
+      </div>
+      <?php endif ?>
+      <!--/.nav-collapse -->
+    </div>
+  </div>
+</div>
--- src/apps/frontend/templates/layout.php
+++ src/apps/frontend/templates/layout.php
@@ -6,9 +6,21 @@
     <?php include_title() ?>
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_stylesheets() ?>
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+      .sidebar-nav {
+        padding: 9px 0;
+      }
+    </style>
     <?php include_javascripts() ?>
   </head>
   <body>
-    <?php echo $sf_content ?>
+  	<?php include_partial('global/nav_bar')?>
+  	<div class="container-fluid">
+    	<?php echo $sf_content ?>
+    </div>
   </body>
 </html>
