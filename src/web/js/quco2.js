e7eb39c08ad5035b9c1af2383fd2a7efc72a5a3c
Marcos Ibañez <mgi1982@gmail.com>
2012-05-02 00:37:08 -0300

    Minor style fixes on the layout to support iframes

--- src/web/js/quco2.js
+++ src/web/js/quco2.js
@@ -1,4 +1,6 @@
-if(top!==self) {
-	document.body.style.paddingTop = 0+'px';
-	document.body.style.paddingBottom = 0+'px';
-}
+$(document).ready(function () {
+	if(top===self) {
+		document.body.style.paddingTop = '60px';
+		document.body.style.paddingBottom = '40px';
+	}
+});
